f7f9f3aa45fdd33c82df3fe78cb3dc71b3823178
Vitaliy <Nesterenko>
2019-04-04 07:42:31 +0500

    Обсуждения с комментариями

--- models/commentfile.js
+++ models/commentfile.js
@@ -1,12 +1,17 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const CommentFile = sequelize.define('CommentFile', {
-    CommentId: DataTypes.INTEGER,
-    file: DataTypes.STRING,
-    size: DataTypes.INTEGER
-  }, {});
+  const CommentFile = sequelize.define(
+    "CommentFile",
+    {
+      CommentId: DataTypes.INTEGER,
+      file: DataTypes.STRING,
+      size: DataTypes.INTEGER
+    },
+    {}
+  );
   CommentFile.associate = function(models) {
     // associations can be defined here
+    models.CommentFile.belongsTo(models.Comment);
   };
   return CommentFile;
 };
